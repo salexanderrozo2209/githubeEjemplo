25410389a988e25b4a25ef652ac816564d80a70b
Sergio Alexander Rozo Olmos <47431734+salexanderrozo2209@users.noreply.github.com>
2021-02-21 19:23:13 -0500

    ARCHIVO_1

---
+++ exmple.py
@@ -0,0 +1,15 @@
+numeros = []
+
+# Le agregamos 3 números
+for i in range(3):
+  numero = float(input("Introduce el número #{}: ".format(i + 1)))
+  numeros.append(numero)
+
+# Asumir que el mayor es el primero de la lista
+mayor = numeros[0]
+# Recorrer y comparar
+for numero in numeros:
+    if numero > mayor:
+        mayor = numero
+# Primer cambio
+print("Mayor:", numero)
